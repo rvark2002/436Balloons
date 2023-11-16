b30f3dd3c37e7c29df37fed00250e64962d4b779
Anvi P <67488590+anvi-p@users.noreply.github.com>
2023-11-15 20:17:14 -0500

    Includes touch event handling

--- project7/app/src/main/java/com/example/project7/MainActivity.kt
+++ project7/app/src/main/java/com/example/project7/MainActivity.kt
@@ -1,33 +1,68 @@
+//Group Members: Anvi Padiyar, Ronith Solipuram, Ritvik Varkhedkar
 package com.example.project7
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
 import android.os.Bundle
 import android.util.Log
+import android.view.MotionEvent
+import android.widget.Toast
 import java.io.InputStream
 import javax.xml.parsers.SAXParser
 import javax.xml.parsers.SAXParserFactory
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
+    private lateinit var balloons : ArrayList<Balloon>
+    private lateinit var gameView : GameView
+    private lateinit var balloonsModel : Balloons
+    private var statusBarHeight : Int = 0
 
-    private lateinit var gameView: GameView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(gameView)
+
+        var statusBarId : Int = resources.getIdentifier("status_bar_height",
+            "dimen", "android")
+        statusBarHeight  = resources.getDimensionPixelSize(statusBarId)
 
         var factory : SAXParserFactory = SAXParserFactory.newInstance()
         var saxParser : SAXParser = factory.newSAXParser()
-
         var handler : SAXHandler = SAXHandler()
-        var iStream : InputStream = resources.openRawResource( R.raw.balloons3 )
+        var iStream : InputStream = resources.openRawResource(R.raw.balloons3) //set to balloons3 when submitting
+        saxParser.parse(iStream, handler)
 
-        // start parsing
-        saxParser.parse( iStream, handler )
+        balloons = handler.getBalloons()
+        balloonsModel = Balloons(balloons.size)
 
-        var balloons : ArrayList<Balloon> = handler.getBalloons()
-        for( balloon in balloons ) {
-            Log.w( "MainActivity", balloon.toString( ) )
+        for(item in balloons){
+            Log.w("MainActivity", item.toString())
         }
+
+        gameView = GameView(this, balloons)
+        setContentView(gameView)
     }
 
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        if (event.action == MotionEvent.ACTION_DOWN && !balloonsModel.checkIfGameOver()) {
+            val x = event.getX().toInt()
+            val y = event.getY().toInt() - statusBarHeight
+            balloonsModel.updateAttempt()
 
+            for (balloon in balloons) {
+                if (!balloon.isPopped() && balloonsModel.isInsideBalloon(x, y, balloon)) {
+                    balloon.pop()
+                    gameView.postInvalidate()
+                    balloonsModel.updatePopped()
+                    if (balloonsModel.checkIfWon()) {
+                        Toast.makeText(
+                            this, "YOU WON!",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+            }
+            if(!balloonsModel.checkIfGameOver() && balloonsModel.checkIfLost()){
+                finish()
+            }
+        }
+        return super.onTouchEvent(event)
+    }
 }
